8b88d3369c5c35a50aeed0636fbbc15efac2eafe
l0rest <jawad.maache@gmail.com>
2024-03-10 18:25:09 +0100

    tp_expr function added

--- main/typing.ml
+++ main/typing.ml
@@ -9,18 +9,41 @@ type environment =
 
 (* TODO: implement *)
 let tp_prog (Prog (fdfs, e)) = let rec getFpdecl = function
-                               (Fundefn(dec,_) :: q) -> let FPdecl(_, n, _) = dec in (n, dec) :: getFpdecl q
+                               | (Fundefn(dec,_) :: q) -> let FPdecl(_, n, _) = dec in (n, dec) :: getFpdecl q
                                | _ -> [] in
 {localvar = []; funbind = getFpdecl fdfs};;
 
 
-(* Recherche d'une variable dans l'environnement *)
+(* Typage d'expressions *)
+let rec tp_expr env exp = match exp with
+| Const (BoolV b) -> BoolT
+| Const (IntV i) -> IntT
+| Const (FloatV f) -> FloatT
+| VarE v -> tp_var env v
+| Binop ((BArith op), e1, e2) -> let t1 = tp_expr env e1 in
+                                 let t2 = tp_expr env e2 in
+                                 if t1 = t2 && t1 = IntT || t1 = FloatT then t1 else raise (TypeError "Arithmetic operation on different types")
+| Binop ((BCompar op), e1, e2) -> let t1 = tp_expr env e1 in
+                                  let t2 = tp_expr env e2 in
+                                  if t1 = t2 then BoolT else raise (TypeError "Comparison operation on different types")
+| Binop ((BLogic op), e1, e2) -> let t1 = tp_expr env e1 in
+                                 let t2 = tp_expr env e2 in
+                                 if t1 = t2 && t1 = BoolT then BoolT else raise (TypeError "Logical operation on different types")
+| IfThenElse (e1,e2,e3) -> let t1 = tp_expr env e1 in
+                           let t2 = tp_expr env e2 in
+                           let t3 = tp_expr env e3 in
+                           if t1 = BoolV && t2 = t3 then t2 else raise (TypeError "IfThenElse")
+| CallE l -> let rec getTypes = function
+             | (x :: q) -> (tp_expr env x) :: getTypes q
+             | _ -> [] in
+             getTypes l
 
-exception NotFound
 
 (* Typage de variables *)
+exception NotFound
+
 let tp_var env valName = let rec searchVal x = function
-                         ((k,v) :: q) -> if k = x then v else searchVal x q
+                         | ((k,v) :: q) -> if k = x then v else searchVal x q
                          | [] -> raise NotFound in
 try searchVal valName env.localvar with NotFound -> let FPdecl (t, _, _) = searchVal valName env.funbind in t;;
 
