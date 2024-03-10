d1fd6d69ac6f295c958b465080d345fc24e19a0b
l0rest <jawad.maache@gmail.com>
2024-03-10 18:47:36 +0100

    tp_prog function improved

--- main/typing.ml
+++ main/typing.ml
@@ -1,20 +1,19 @@
 open Lang
 
 (* Environments *)
-
 type environment =
     {localvar: (vname * tp) list;
      funbind: (vname * fpdecl) list}
 
 
-(* TODO: implement *)
+(* Program typing *)
 let tp_prog (Prog (fdfs, e)) = let rec getFpdecl = function
-                               | (Fundefn(dec,_) :: q) -> let FPdecl(_, n, _) = dec in (n, dec) :: getFpdecl q
+                               | (dec :: q) -> (name_of_fpdecl n, dec) :: getFpdecl q
                                | _ -> [] in
-{localvar = []; funbind = getFpdecl fdfs};;
+{localvar = []; funbind = getFpdecl fdfs}
 
 
-(* Typage d'expressions *)
+(* Expression typing *)
 let rec tp_expr env exp = match exp with
 | Const (BoolV b) -> BoolT
 | Const (IntV i) -> IntT
@@ -39,11 +38,10 @@ let rec tp_expr env exp = match exp with
              getTypes l
 
 
-(* Typage de variables *)
+(* Variable typing *)
 exception NotFound
 
 let tp_var env valName = let rec searchVal x = function
                          | ((k,v) :: q) -> if k = x then v else searchVal x q
                          | [] -> raise NotFound in
-try searchVal valName env.localvar with NotFound -> let FPdecl (t, _, _) = searchVal valName env.funbind in t;;
-
+try searchVal valName env.localvar with NotFound -> let FPdecl (t, _, _) = searchVal valName env.funbind in t
