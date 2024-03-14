4e175865d2dbdc66881aa10d42f873c54bc6c250
L0rest <jawad.maache@gmail.com>
2024-03-14 22:34:39 +0100

    is_tailrec_expr function added

--- main/transf.ml
+++ main/transf.ml
@@ -1,4 +1,4 @@
-  (* Transformation of Caml code to Python code *)
+(* Transformation of Caml code to Python code *)
 
 open Lang
 
@@ -11,3 +11,13 @@ let rec names_expr = StringSet.empty
 (* TODO: implement *)
 let transf_prog (Prog(fdfs, e)) = Prog(fdfs, e)
 
+
+let rec is_tailrec_expr fname e = match e with
+| VarE v -> v != fname
+| BinOp(_, e1, e2) -> is_tailrec_expr fname e1 && is_tailrec_expr fname e2
+| IfThenElse(cond, e1, e2) -> is_tailrec_expr fname cond && is_tailrec_expr fname e1 && is_tailrec_expr fname e2
+| CallE l -> let rec is_tailrec_call = function
+             | (a :: q) -> is_tailrec_expr fname a && is_tailrec_call q
+             | _ -> true
+             in is_tailrec_call l
+| _ -> true
