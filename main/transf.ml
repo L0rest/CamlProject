99e33a3b20f0d389cb6ea2aef84501f00ea7bb5e
Hiolia <69538503+Hiolia@users.noreply.github.com>
2024-03-15 16:32:17 +0100

    Update transf.ml
    first tries at part 2.5

--- main/transf.ml
+++ main/transf.ml
@@ -8,9 +8,6 @@ module StringSet = Set.Make(String)
 (* TODO: implement *)
 let rec names_expr = StringSet.empty
 
-(* TODO: implement *)
-let transf_prog (Prog(fdfs, e)) = Prog(fdfs, e)
-
 
 let rec is_tailrec_expr fname e = match e with
 | VarE v -> v != fname
@@ -21,3 +18,18 @@ let rec is_tailrec_expr fname e = match e with
              | _ -> true
              in is_tailrec_call l
 | _ -> true
+
+let rec transf_expr fname paraml e = match e with
+| IfThenElse(cond, e1, e2) ->
+      if is_tailrec_expr fname cond then
+          IfThenElse (transf_expr fname paraml cond, transf_expr fname paraml e1, transf_expr fname paraml e2)
+      else e
+(* Not working *)
+|CallE l -> if List.exists(fun x -> not (is_tailrec_expr fname x)) l then e else Assign(paraml, l)
+| _ -> Return e
+
+(* TODO: implement *)
+let transf_fpdefn (FPdecl(tp, fname, params), e) =
+
+
+let transf_prog (Prog(fdfs, e)) = let new_fdfs = List.map transf_fpdefn fdfs in Prog(new_fdfs, e)
