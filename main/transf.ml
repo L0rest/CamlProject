87c770205f2b1b69f2ef2c3ec251f61d5800150c
Hiolia <69538503+Hiolia@users.noreply.github.com>
2024-03-22 16:13:57 +0100

    hope it works

--- main/transf.ml
+++ main/transf.ml
@@ -4,30 +4,38 @@ open Lang
 
 module StringSet = Set.Make(String)
 
-
-(* TODO: implement *)
-let rec names_expr e = StringSet.empty
+let rec names_expr  = function
+    Const(_) -> StringSet.empty
+   | VarE v -> StringSet.singleton v
+   | BinOp(_, e1, e2) -> StringSet.union (names_expr e1) (names_expr e2)
+   | IfThenElse(cond, e1, e2) -> StringSet.union (names_expr cond) (StringSet.union (names_expr e1) (names_expr e2))
+   | CallE (f::args) -> List.fold_left (fun acc a -> StringSet.union acc (names_expr a)) (names_expr f) args
+   | _ -> StringSet.empty
 
 let rec is_tailrec_expr fname e = match e with
-| VarE v -> v != fname
+ VarE v -> v != fname
 | BinOp(_, e1, e2) -> is_tailrec_expr fname e1 && is_tailrec_expr fname e2
-| IfThenElse(cond, e1, e2) -> is_tailrec_expr fname cond && is_tailrec_expr fname e1 && is_tailrec_expr fname e2
-| CallE (f::args) -> List.for_all (is_tailrec_expr fname) args
-| _ -> true
+| IfThenElse(cond, e1, e2) -> not (StringSet.mem fname (names_expr cond)) && is_tailrec_expr fname e1 && is_tailrec_expr fname e2
+| CallE (f::args) -> List.for_all (fun  a -> not (StringSet.mem fname (names_expr a))) args
+| e -> not (StringSet.mem fname (names_expr e))
 
 let rec transf_expr fname paraml e = match e with
-| IfThenElse(cond, e1, e2) ->
-      if is_tailrec_expr fname cond then
-          IfThenElse (transf_expr fname paraml cond, transf_expr fname paraml e1, transf_expr fname paraml e2)
-      else e
-(*need to patch *)
-|CallE l -> if List.exists(fun x -> not (is_tailrec_expr fname x)) l then e
-else CallE (List.map (transf_expr fname paraml) l)
-| _ -> e
+    | CallE (VarE f'::args) when f' = fname ->
+        let assignments = List.map2 (fun param arg -> Assign([param], [arg])) paraml args in
+        let seq_assignments = List.fold_right (fun a b -> Seq(a, b)) assignments Skip in
+        While (Const (BoolV true), seq_assignments)
+    | IfThenElse(cond, e1, e2) ->
+        Cond (cond, transf_expr fname paraml e1, transf_expr fname paraml e2)
+    | _ -> Return e
 
-(* TODO: implement *)
-let transf_fpdefn (FPdecl(tp, fname, params), e) = (FPdecl(tp, fname, params), e)
+let transf_fpdefn (FPdecl(tp, fname, params), e) =
+    let param_names = List.map (fun (Vardecl(vname, _)) -> vname) params in
+    let new_e = transf_expr fname param_names e in
+    (FPdecl(tp, fname, params), new_e)
 
-(* Not sure *)
-let transf_prog (Prog(fdfs, e)) = IntT
-
+let transf_fpdefn fpdefn =
+    match fpdefn with
+    | (FPdecl(tp, fname, params), e) ->
+        let param_names = List.map (fun (Vardecl(vname, _)) -> vname) params in
+        let new_e = transf_expr fname param_names e in
+        (FPdecl(tp, fname, params), new_e)
