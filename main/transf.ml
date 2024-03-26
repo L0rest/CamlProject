d99d2f5dbd1bae6eec9c2f71167ef846d9a5f048
L0rest <jawad.maache@gmail.com>
2024-03-26 19:05:25 +0100

    Translator patched

--- main/transf.ml
+++ main/transf.ml
@@ -5,7 +5,7 @@ open Lang
 module StringSet = Set.Make(String)
 
 
-let rec names_expr  = function
+let rec names_expr = function
     Const(_) -> StringSet.empty
    | VarE v -> StringSet.singleton v
    | BinOp(_, e1, e2) -> StringSet.union (names_expr e1) (names_expr e2)
@@ -15,10 +15,8 @@ let rec names_expr  = function
 
 
 let rec is_tailrec_expr fname e = match e with
- VarE v -> v != fname
-| BinOp(_, e1, e2) -> is_tailrec_expr fname e1 && is_tailrec_expr fname e2
 | IfThenElse(cond, e1, e2) -> not (StringSet.mem fname (names_expr cond)) && is_tailrec_expr fname e1 && is_tailrec_expr fname e2
-| CallE (f::args) -> List.for_all (fun  a -> not (StringSet.mem fname (names_expr a))) args
+| CallE (f::args) -> List.for_all (fun a -> not (StringSet.mem fname (names_expr a))) args
 | e -> not (StringSet.mem fname (names_expr e))
 
 
