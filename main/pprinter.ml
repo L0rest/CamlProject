65c17b1ff2ffb592d694cd398c4c6e385d3cdf08
L0rest <jawad.maache@gmail.com>
2024-03-27 13:07:27 +0100

    Pprinter improved

--- main/pprinter.ml
+++ main/pprinter.ml
@@ -52,7 +52,6 @@ let rec doc_of_expr = function
 | Const c -> doc_of_const c
 | VarE v -> doc_of_var v
 | BinOp (bop, e1, e2) -> parens (doc_of_expr e1 ^^ space ^^ doc_of_binop bop ^^ space ^^ doc_of_expr e2)
-(* See https://docs.python.org/3/reference/expressions.html#conditional-expressions *)
 | IfThenElse(cond, t, e) -> parens (doc_of_expr t ^^ space ^^ string "if" ^^ space ^^ doc_of_expr cond ^^ space ^^ string "else" ^^ space ^^ doc_of_expr e)
 | CallE(VarE(fn)::es) -> doc_of_var fn ^^ doc_of_expr_list es
 | CallE(_) -> empty and doc_of_expr_list es = parens (separate_map comma doc_of_expr es)
@@ -63,14 +62,13 @@ let rec doc_of_cmd = function
 | Skip -> empty
 | Assign(vs, es) -> doc_of_var_list vs ^^ equals ^^ doc_of_expr_list es
 | Seq(c1, c2) -> doc_of_cmd c1 ^^ doc_of_cmd c2
-| Cond(e, c1, c2) -> (separate space [string "if"; doc_of_expr e; colon]) ^^
+| Cond(e, c1, c2) -> string "if" ^^ space ^^ doc_of_expr e ^^ colon ^^
                      (nest indent_level (hardline ^^ (doc_of_cmd c1))) ^^
                      (match c2 with
-                      Skip -> empty
-                      | _ -> hardline ^^
-                             (separate space [string "else"; colon]) ^^
-                             (nest indent_level (hardline ^^ (doc_of_cmd c2))))
-| While(e, c) -> (separate space [string "while"; doc_of_expr e; colon]) ^^
+                     | Skip -> empty
+                     | _ -> hardline ^^ string "else" ^^ colon ^^
+                            (nest indent_level (hardline ^^ (doc_of_cmd c2))))
+| While(e, c) -> string "while" ^^ space ^^ doc_of_expr e ^^ colon ^^
                  (nest indent_level (hardline ^^ (doc_of_cmd c)))
 | Return(e) -> (separate space [string "return"; doc_of_expr e])
 
@@ -78,16 +76,18 @@ let rec doc_of_cmd = function
 (* Function and procedure definitions printing *)
 let doc_of_fpdefn  = function
 | Fundefn(FPdecl(rt, fn, params), e)->
-      (separate space [string "def"; string fn; (doc_of_var_list (List.map name_of_vardecl params)); colon]) ^^
-      (nest indent_level (hardline ^^ (string "return"  ^^ parens (doc_of_expr e))))
+      (string "def " ^^ string fn ^^ doc_of_var_list (List.map name_of_vardecl params) ^^ colon) ^^
+      (nest indent_level (hardline ^^ (string "return"  ^^ doc_of_expr e))) ^^
+      hardline
 | Procdefn(FPdecl(rt, fn, params), c) ->
-      (separate space [string "def"; string fn; (doc_of_var_list (List.map name_of_vardecl params)); colon]) ^^
-      (nest indent_level (hardline ^^ (doc_of_cmd c)))
+      (string "def " ^^ string fn ^^ doc_of_var_list (List.map name_of_vardecl params) ^^ colon) ^^
+      (nest indent_level (hardline ^^ (doc_of_cmd c))) ^^
+      hardline
 
 
 (* Program printing *)
 let doc_of_prog (Prog(fdfs, e)) = (separate_map hardline doc_of_fpdefn fdfs) ^^ hardline ^^
-(string "print" ^^ (parens (string "str" ^^ (parens (doc_of_expr e))))) ^^ hardline
+(string "print" ^^ (parens (doc_of_expr e))) ^^ hardline
 
 
 (* Printing to channel *)
